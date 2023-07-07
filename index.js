2774489b66f5659eb9edd2b7e37b73b5194cb44d
3anza <salmaaldubouni@gmail.com>
2023-07-07 10:17:31 +0200

    done

--- index.js
+++ index.js
@@ -16,8 +16,64 @@ mongoose
     return Recipe.deleteMany()
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    return Recipe.create({
+      title: "Lemon Tiramisu",
+      level: "Easy Peasy",
+      ingredients: ["Lemons", "Eggs", "Yolk", "Mascarpone", "Heavy Whipping Cream", "Limoncello", "Basil"],
+      cuisine: "Italian",
+      dishType: "dessert",
+      image: "https://flouringkitchen.com/wp-content/uploads/2022/08/BW1A5918.jpg",
+      duration: 60,
+      creator: "Mary",
+      created: Date.now()
+    })
   })
+  .then((newRecipe) => {
+    console.log("new recipe saved to database:", newRecipe)
+  })
+
+  .then(() => {
+    return Recipe.insertMany(data)
+  })
+  .then((recipes) => {
+    recipes.forEach((recipe) => {
+      console.log("Recipe title:", recipe.title)
+    })
+  })
+
+  .then(() => {
+    return Recipe.findOneAndUpdate({
+      id: "64a6c610363b36bec9831410" ,
+      duration: 100,
+      upsert: true }
+      )
+  })
+  .then(() => {
+    console.log("Rigatoni alla Genovese recipe updated successfully to duration of 100 minutes!")
+  })
+
+  .then(() => {
+    return Recipe.deleteOne({
+      title: "Carrot Cake"
+    })
+  })
+
+  .then(() => {
+    console.log("Carrot Cake recipe deleted successfully!")
+  })
+
+
+    .catch(error => {
+      console.error('Error connecting to the database', error);
+    })
+
+  .finally(() => {
+    mongoose.connection.close()
+      .then(() => {
+      console.log("database successfully closed!")
+    })
+  })
+
   .catch(error => {
-    console.error('Error connecting to the database', error);
+    console.error("Error disconnecting the database", error);
   });
